a91f5380e6d67d252fd4afabefd1b6a4df81d9d2
AditSembiring <adit3gmg@gmail.com>
2024-05-30 17:11:28 +0700

    praktikum 8

---
+++ praktikum8/src/components/Artikelhome.vue
@@ -0,0 +1,11 @@
+<template>
+    <img alt="vie img" class="logo" src="@/assets/vuejs.svg" width="500" height="250" />
+    <div>
+        <h1>Ini adalah halaman artikel home</h1>
+        <p>Isi artikel xixixixi</p>
+        <p>Isi artikel xixixixi</p>
+        <p>Isi artikel xixixixi</p>
+        <p>Isi artikel xixixixi</p>
+
+    </div>
+</template>
---
+++ praktikum8/src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+import Artikelhome from '../components/Artikelhome.vue'
+
+</script>
+
+<template>
+  <main>
+    <Artikelhome/>
+  </main>
+</template>
